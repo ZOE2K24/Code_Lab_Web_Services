2b89dd5dbebc410eac2618155548db1a4235acb2
ZOE2K24 <jm.legaspi06@gmail.com>
2025-03-20 19:44:59 -0600

    Create pages.js

---
+++ pages.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+
+//juan miguel legaspi (mid-code signature)
+
+router.get("/", (req, res) =>{
+    res.send("This is the home page!");
+});
+
+router.get("/about", (req, res) => {
+    res.send("This is the about page!");
+});
+
+modules.exports = router;
